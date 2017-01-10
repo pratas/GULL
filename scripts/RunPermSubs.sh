18cdccacfd7c6315920dbede4904ec95148b2569
pratas <pratas@ua.pt>
2017-01-10 15:46:20 +0000

    version1x

--- scripts/RunPermSubs.sh
+++ scripts/RunPermSubs.sh
@@ -4,7 +4,7 @@
 GET_GULL=0;
 GET_XS=0;
 GET_GOOSE=0;
-RUN_XS=1;
+RUN_XS=0;
 RUN_GULL=1;
 ###############################################################################
 # GET GOOSE ===================================================================
@@ -43,7 +43,7 @@ if [[ "$RUN_XS" -eq "1" ]]; then
   PMIN=0;
   PMAX=20;
   JUMP=1;
-  ./XS -v -ls 1000 -n 10000 -f 0.25,0.25,0.25,0.25,0 -eh -eo -edb -es -s 0 SAMPLE
+  ./XS -v -ls 1000 -n 5000 -f 0.25,0.25,0.25,0.25,0 -eh -eo -edb -es -s 0 SAMPLE
   for((x=$PMIN ; x<=$PMAX ; x+=$JUMP));
     do
     echo "Permutation rate: $x";
@@ -55,7 +55,8 @@ fi
 ###############################################################################
 # RUN GULL ====================================================================
 if [[ "$RUN_GULL" -eq "1" ]]; then
-  ./GULL-map -v -n 2 -l 10 -c 5 -x Matrix.csv SAMPLE0.mut:SAMPLE1.mut:SAMPLE2.mut:SAMPLE3.mut:SAMPLE4.mut:SAMPLE5.mutSAMPLE6.mut:SAMPLE7.mut:SAMPLE8.mut:SAMPLE9.mut:SAMPLE10.mut:SAMPLE11.mut:SAMPLE12.mut:SAMPLE13.mut:SAMPLE14.mut:SAMPLE15.mut:SAMPLE16.mut:SAMPLE17.mut:SAMPLE18.mut:SAMPLE19.mut:SAMPLE20.mut
+  ./GULL-map -v -n 4 -l 10 -c 10 -x Matrix.csv SAMPLE0.mut:SAMPLE1.mut:SAMPLE2.mut:SAMPLE3.mut:SAMPLE4.mut:SAMPLE5.mut:SAMPLE6.mut:SAMPLE7.mut:SAMPLE8.mut:SAMPLE9.mut:SAMPLE10.mut:SAMPLE11.mut:SAMPLE12.mut:SAMPLE13.mut:SAMPLE14.mut:SAMPLE15.mut:SAMPLE16.mut:SAMPLE17.mut:SAMPLE18.mut:SAMPLE19.mut:SAMPLE20.mut
+  ./GULL-visual -v -x SynMap.svg Matrix.csv
 fi
 ###############################################################################
 
